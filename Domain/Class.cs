c320f5fadb6724ca36249508cb5c08e1a04be897
ViaKatia <113637269+ViaKatia@users.noreply.github.com>
2023-11-07 10:38:21 +0100

    Domain

---
+++ Domain/Class.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+
+public class Class
+{
+    
+    public string Name { get; set; }
+    public Teacher Teacher { get; set; }
+    public List<Student> Students = new List<Student>();
+
+    Class(Teacher teacher, List<Student> students, string name)
+    {
+        Teacher = teacher;
+        Students = students;
+        Name = name;
+    }
+
+
+}
---
+++ Domain/Exam.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+
+public class Exam
+{
+    
+    public string NameOfExam { get; set; }
+    public int Grade { get; set; }
+    public DateTime Dt = new DateTime();
+    public Class Class { get; set; }
+
+    Exam(string nameOfExam, int grade, DateTime dt, Class class1)
+    {
+        NameOfExam = nameOfExam;
+        Grade = grade;
+        Dt = dt;
+        Class = class1;
+    }  
+}
