3cd12d5a0976c4d1f9a1f4dfd2e6ad5f65478d0e
Dominika Krullova <60384405+DominikaKrullova@users.noreply.github.com>
2023-11-07 13:08:22 +0100

    Fix

---
+++ Domain/Class.cs
@@ -0,0 +1,16 @@
+namespace Domain;
+
+public class Class
+{
+    public string Name { get; set; }
+    public Teacher Teacher { get; set; }
+    public List<Student> Students = new List<Student>();
+
+    Class(Teacher teacher, List<Student> students, string name)
+    {
+        Teacher = teacher;
+        Students = students;
+        Name = name;
+    }
+    
+}
---
+++ Domain/Exam.cs
@@ -0,0 +1,17 @@
+namespace Domain;
+
+public class Exam
+{
+    public string NameOfExam { get; set; }
+    public int Grade { get; set; }
+    public DateTime Dt = new DateTime();
+    public Class Class { get; set; }
+
+    Exam(string nameOfExam, int grade, DateTime dt, Class class1)
+    {
+        NameOfExam = nameOfExam;
+        Grade = grade;
+        Dt = dt;
+        Class = class1;
+    }
+}
