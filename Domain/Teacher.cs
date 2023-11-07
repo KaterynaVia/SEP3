09df7fa1c54f29bcdc1e2fcbf13c58d174d22ee0
ViaKatia <113637269+ViaKatia@users.noreply.github.com>
2023-11-07 10:38:21 +0100

    Domain

---
+++ Domain/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class Teacher : User
+{
+    public string Name { get; set; }
+    public Teacher(string name, string password, int id) : base(password, id)
+    {
+        Name = name;
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain;
+
+public class User
+{
+    
+    public  int Id { get; set; }
+    public string Password { get; set; }
+
+    public User(string password, int id)
+    {
+        Id = id;
+        Password = password;
+    }
+}
