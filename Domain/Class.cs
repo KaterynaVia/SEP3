6152dcc81a49ab53a262837e2e4bee83e9c81844
Dominika Krullova <60384405+DominikaKrullova@users.noreply.github.com>
2023-11-22 10:39:41 +0100

    fix

--- Domain/Class.cs
+++ Domain/Class.cs
@@ -5,12 +5,20 @@ public class Class
     public string Name { get; set; }
     public Teacher Teacher { get; set; }
     public List<Student> Students = new List<Student>();
+    public int Id { get; set; }
 
+    public Class(Teacher teacher, List<Student> students, string name, int id)
+    {
+        Teacher = teacher;
+        Students = students;
+        Name = name;
+        Id = id;
+    }
+    
     public Class(Teacher teacher, List<Student> students, string name)
     {
         Teacher = teacher;
         Students = students;
         Name = name;
     }
-    
 }
